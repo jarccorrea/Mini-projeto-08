def14ad3ce11546bc7f1fd65d397e37447ee694b
Jorge Correa <38728415+jarccorrea@users.noreply.github.com>
2025-06-17 16:44:28 -0300

    Update lista_tarefas.py

--- lista_tarefas.py
+++ lista_tarefas.py
@@ -1,43 +1,94 @@
-lista_tarefas = []
+import json
+import os
+
+### Funções ###
+def listar (tarefas):
+    print ()
+    if not tarefas:
+        print ('Nenhuma tarefa para listar.')
+        print()
+        return
+    print('\nTarefas:')
+    for i, tarefa in enumerate(tarefas, start=1):
+        print(f'{i}. {tarefa}')
+    print()
+
+def desfazer (tarefas, tarefas_desfeitas):
+    print ()
+    if not tarefas:
+        print ('Nenhuma tarefa para desfazer.')
+        print()
+    else:
+        tarefa = tarefas.pop()
+        tarefas_desfeitas.append(tarefa)
+        print(f'Tarefa: "{tarefa}" desfeita.')
+        print()
+
+def refazer(tarefas, tarefas_desfeitas):
+    print()
+    if not tarefas_desfeitas:
+        print('Nenhuma tarefa para refazer.')
+        print()
+        return
+    tarefa = tarefas_desfeitas.pop()
+    tarefas.append(tarefa)
+    print(f'Tarefa: "{tarefa}" refeita.')
+    print()
+
+def adicionar(tarefa, tarefas):
+    print ()
+    tarefa = tarefa.strip()
+    if not tarefa:
+        print ('Você não digitou uma tarefa.')
+        return
+    print (f'Tarefa: {tarefa} adicionada a lista com sucesso.')
+    tarefas.append(tarefa)
+    print ()
+    print (tarefas)
+
+def ler (caminho_arquivo):
+    dados = []
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('\nArquivo não encontrado. Criando novo arquivo...\n')
+        salvar([], caminho_arquivo)
+    except json.JSONDecodeError:
+        print('\nArquivo vazio ou corrompido. Iniciando lista vazia...\n')
+    return dados
+
+def salvar (tarefas, caminho_arquivo):
+    dados = tarefas
+    with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+        dados = json.dump(tarefas, arquivo, indent=2, ensure_ascii=False)
+
+def clear_console():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+### Variaveis ###
+CAMINHO_ARQUIVO = 'aula56.json'
+
+### Listas ###
+tarefas = ler(CAMINHO_ARQUIVO)
 tarefas_desfeitas = []
 
 while True:
-    print ('Comandos: Listar, Desfazer, Refazer e Sair')
+    print('Comandos: listar, desfazer, refazer,clear e sair')
     tarefa = input('Digite uma tarefa ou comando: ')
 
-    if tarefa == 'listar' or tarefa == 'Listar' or tarefa == 'LISTAR':
-        if not lista_tarefas:
-            print ('Nenhuma tarefa na lista.')
-            print ()
-        else:
-            print ('\nTarefas:')
-        for i, tarefa in enumerate(lista_tarefas, start=1):
-            print (f'{i}. {tarefa}')
-        print()
-
-    elif tarefa == 'desfazer' or tarefa == 'Desfazer' or tarefa == 'DESFAZER':
-        if lista_tarefas:
-            tarefa = lista_tarefas.pop()
-            tarefas_desfeitas.append(tarefa)
-            print (f'Tarefa: "{tarefa}" desfeita.')
-            print()
-        else:
-            print ('Nenhuma tarefa para desfazer.')
-            print()
-
-    elif tarefa == 'refazer' or tarefa == 'Refazer' or tarefa == 'REFAZER':
-        if tarefas_desfeitas:
-            tarefa = tarefas_desfeitas.pop()
-            lista_tarefas.append(tarefa)
-            print (f'Tarefa: "{tarefa}" refeita.')
-            print()
-        else:
-            print ('Nehuma tarefa para refazer')
-            print()
-
-    elif tarefa == 'sair' or tarefa == 'Sair' or tarefa == 'SAIR':
+    if tarefa == 'sair':
+        print ('Saindo do programa...')
         break
 
-    else:
-        lista_tarefas.append(tarefa)
-        print()
+    comandos = {
+        'listar': lambda: listar(tarefas),
+        'desfazer': lambda: desfazer(tarefas, tarefas_desfeitas),
+        'refazer': lambda: refazer(tarefas, tarefas_desfeitas),
+        'clear': lambda: clear_console(),
+        'adicionar': lambda: adicionar(tarefa, tarefas),
+    }
+    comando = comandos.get(tarefa) if comandos.get(tarefa) is not None else \
+        comandos['adicionar']
+    comando()
+    salvar(tarefas, CAMINHO_ARQUIVO)
